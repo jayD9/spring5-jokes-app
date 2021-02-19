b9b7207d4fa218b18671340d4b54ad147fcbee36
Jay Darji <jaydarji369@gmail.com>
2021-02-19 02:13:52 -0500

    Java configuration example

---
+++ src/main/java/jd/springframework/joke/jokeapp/config/ChuckConfig.java
@@ -0,0 +1,18 @@
+package jd.springframework.joke.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * author JD
+ * created 2/19/21 1:50 AM
+ */
+@Configuration
+public class ChuckConfig {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
--- src/main/java/jd/springframework/joke/jokeapp/service/JokeServiceImpl.java
+++ src/main/java/jd/springframework/joke/jokeapp/service/JokeServiceImpl.java
@@ -1,6 +1,7 @@
 package jd.springframework.joke.jokeapp.service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,8 +13,8 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
